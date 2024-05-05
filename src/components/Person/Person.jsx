9c14b01f9ac62358e81629ea25e83d98b43daacd
Alex Pakhomov <pakhomov.alex.v@gmail.com>
2024-05-05 15:10:44 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+import classNames from 'classnames';
+
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    <p className={classNames('', { Person__age: person.age })}>
+      {person.age ? <p>I am {person.age}</p> : <></>}
+    </p>
+    <p className={classNames('', { Person__partner: person.partnerName })}>
+      {person.isMarried === true ? (
+        <p>
+          {person.partnerName} is my
+          {person.sex === 'm' ? ' wife' : ' husband'}
+        </p>
+      ) : (
+        <p>I am not married</p>
+      )}
+    </p>
+  </section>
+);
